4822f835eb57a5b6cb4152bf936452d3dc3fc998
Maarten Bicknese <maarten.bicknese@gmail.com>
2023-12-05 15:52:53 +0100

    feat(2023): solve day 3 part 2

--- src/pages/day3.ts
+++ src/pages/day3.ts
@@ -6,57 +6,88 @@ import input from "../inputs/day3.txt?raw";
  * The first arguments value will be used as size and distance is calculated for all edges, the shortest
  * distance is returned.
  */
-function distance(a: { x: number, y: number, value: string }, b: { x: number, y: number }): number {
-	const distances = Array.from(Array(a.value.length), (_, offset) => Math.max(Math.abs((a.x + offset) - b.x), Math.abs(a.y - b.y)));
+function distance(a: { x: number; y: number; value: string }, b: { x: number; y: number }): number {
+	const distances = Array.from(Array(a.value.length), (_, offset) =>
+		Math.max(Math.abs(a.x + offset - b.x), Math.abs(a.y - b.y)),
+	);
 
 	return Math.min(...distances);
 }
 
 async function solve1() {
-	document.querySelector('#part1')!.innerHTML = '<p>calculating</p>';
-	const numbers: { x: number, y: number, value: string, isPart?: boolean }[] = [];
-	const symbols: { x: number, y: number}[] = [];
+	document.querySelector("#part1")!.innerHTML = "<p>calculating</p>";
+	const numbers: { x: number; y: number; value: string; isPart?: boolean }[] = [];
+	const symbols: { x: number; y: number }[] = [];
 	let inNumber = false;
-	input.split("\n").forEach((line, y) => line.split("").forEach((char, x) => {
-		if (/[0-9]/.test(char)) {
-			if (inNumber) {
-				numbers[numbers.length-1].value += char;
+	input.split("\n").forEach((line, y) =>
+		line.split("").forEach((char, x) => {
+			if (/[0-9]/.test(char)) {
+				if (inNumber) {
+					numbers[numbers.length - 1].value += char;
+				} else {
+					numbers.push({ x, y, value: char });
+					inNumber = true;
+				}
 			} else {
-				numbers.push({ x, y, value: char });
-				inNumber = true;
+				inNumber = false;
+				if (char !== ".") {
+					symbols.push({ x, y });
+				}
 			}
-		} else {
-			inNumber = false;
-			if (char !== '.') {
-				symbols.push({ x, y });
-			}
-		}
+		}),
+	);
+
+	const partNumbers = numbers.map((number) => ({
+		...number,
+		isPart: symbols.reduce((prev, symbol) => distance(number, symbol) === 1 || prev, false),
 	}));
-
-	const partNumbers = numbers
-		.map(number => ({
-			...number,
-			isPart: symbols.reduce((prev, symbol) => distance(number, symbol) === 1 || prev, false)
-		}));
-
 
 	const debug = false;
 	if (debug) {
-		document.querySelector('#part1')!.innerHTML = `
+		document.querySelector("#part1")!.innerHTML = `
 			<ul>
-			${partNumbers.map(({x,y,value,isPart}) => `<li>${x}, ${y}: ${value} ${isPart ? '*' : ''}</li>`).join("")}
+			${partNumbers.map(({ x, y, value, isPart }) => `<li>${x}, ${y}: ${value} ${isPart ? "*" : ""}</li>`).join("")}
 			</ul>
 		`;
 	} else {
 		const sum = partNumbers.filter(({ isPart }) => isPart).reduce((sum, { value }) => sum + Number(value), 0);
-		document.querySelector('#part1')!.innerHTML = `
+		document.querySelector("#part1")!.innerHTML = `
 			<p>${sum}</p>
 		`;
 	}
 }
 
 function solve2() {
-	return 'wip'
+	const numbers: { x: number; y: number; value: string }[] = [];
+	const symbols: { x: number; y: number; gears: string[] }[] = [];
+	let inNumber = false;
+	input.split("\n").forEach((line, y) =>
+		line.split("").forEach((char, x) => {
+			if (/[0-9]/.test(char)) {
+				if (inNumber) {
+					numbers[numbers.length - 1].value += char;
+				} else {
+					numbers.push({ x, y, value: char });
+					inNumber = true;
+				}
+			} else {
+				inNumber = false;
+				if (char === "*") {
+					symbols.push({ x, y, gears: [] });
+				}
+			}
+		}),
+	);
+
+	const ratios = symbols
+		.map((symbol) => ({
+			...symbol,
+			gears: numbers.filter((number) => distance(number, symbol) === 1).map(({ value }) => value),
+		}))
+		.filter(({ gears }) => gears.length === 2)
+		.map(({ gears }) => Number(gears[0]) * Number(gears[1]));
+
+	return ratios.reduce((sum, cur) => sum + cur, 0);
 }
 
 export function render() {
