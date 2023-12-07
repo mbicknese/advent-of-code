34d676b77ae7c75014bddfff143efa1e5f7bc1a9
Maarten Bicknese <maarten.bicknese@gmail.com>
2023-12-07 08:27:38 +0100

    feat(2023): solve day 6, part 2

--- src/pages/day6.ts
+++ src/pages/day6.ts
@@ -1,10 +1,9 @@
 import input from "../inputs/day6.txt?raw";
 
-const times = [...input.split("\n")[0].matchAll(/\d+/g)].map((time) => Number(time));
-const distances = [...input.split("\n")[1].matchAll(/\d+/g)].map((distance) => Number(distance));
-
 // Not the most elegant, but sure gets there without too much memory usage
 function solve1() {
+	const times = [...input.split("\n")[0].matchAll(/\d+/g)].map((time) => Number(time));
+	const distances = [...input.split("\n")[1].matchAll(/\d+/g)].map((distance) => Number(distance));
 	const wins = times.map((time, race) => {
 		const low = (() => {
 			for (let i = 1; i < time; ++i) {
@@ -28,7 +27,27 @@ function solve1() {
 }
 
 function solve2() {
-	return "wip";
+	const time = Number([...input.split("\n")[0].matchAll(/\d+/g)].join(""));
+	const distance = Number([...input.split("\n")[1].matchAll(/\d+/g)].join(""));
+
+	const low = (() => {
+		for (let i = 1; i < time; ++i) {
+			if (distance < i * (time - i)) {
+				return i;
+			}
+		}
+		return 0;
+	})();
+	const high = (() => {
+		for (let i = time - 1; i > 0; --i) {
+			if (distance < i * (time - i)) {
+				return i;
+			}
+		}
+		return 0;
+	})();
+
+	return high - low + 1;
 }
 
 export function render() {
